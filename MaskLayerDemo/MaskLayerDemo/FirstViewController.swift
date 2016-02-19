2968fed59ed08606f20d3661856506ed64c89443
Victor Chee <victor.qhj@gmail.com>
2016-02-19 14:26:38 +0800

    add mask layer

--- MaskLayerDemo/MaskLayerDemo/FirstViewController.swift
+++ MaskLayerDemo/MaskLayerDemo/FirstViewController.swift
@@ -9,10 +9,16 @@
 import UIKit
 
 class FirstViewController: UIViewController {
+    @IBOutlet weak var button: UIButton!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        let path = UIBezierPath(roundedRect: button.bounds, byRoundingCorners: [UIRectCorner.BottomLeft, .TopRight], cornerRadii: CGSize(width: 50, height: 0))
+        let maskLayer = CAShapeLayer()
+        maskLayer.path = path.CGPath
+        button.layer.mask = maskLayer
     }
 
     override func didReceiveMemoryWarning() {
